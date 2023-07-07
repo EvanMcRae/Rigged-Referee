6847c950f068871c8b2906444150a863e224832a
Awsomenova1 <91033039+Awsomenova1@users.noreply.github.com>
2023-07-07 17:52:55 -0400

    Added pseudocode to GameControler

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,13 @@ public class GameController : MonoBehaviour
     public int currentLevel = 0;
     public static GameController instance;
 
+    public int fighterOneScore;
+    public int fighterTwoScore;
+    public int scoreToWin;
+
+    public int suspicion;
+    public int susToLose;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,5 +36,43 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    }
+
+    //awards points to a figther based on the player's command
+    void AwardScore(){
+        //if f1 button
+        //fighterOneScore += 1;
+        //if f2 button
+        //fighterTwoScore +=2;
+
+        //if suspicous
+        //add suspicion
+
+        //CheckVictory()
+    }
+
+    //check if the player's current action is suspicious, add apropriate suspicion if so
+    void CheckSusAction(){
+        //make if statements for fighters
+        
+        //if player 1 awared and player 1 hit, add 0%
+        //if player 1 blocked, add 20%
+        //if player 1 wiffed, add 40%
+        //if player 1 didn't punch, add 80%
+
+        //same for player 2
+    }
+
+    //decide if the player won or lost
+    void CheckVictory(){
+        if(suspicion >= susToLose){
+            //lose
+        }
+        else if(fighterOneScore >= scoreToWin){
+            //win
+        }
+        else if(fighterTwoScore >= scoreToWin){
+            //lose
+        }
     }
 }
