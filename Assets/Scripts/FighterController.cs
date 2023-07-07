963660408e6fa23b1bf5c9c5cd917466c6633890
EvanMcRae <evanmmcrae@gmail.com>
2023-07-07 17:54:10 -0400

    Add sound players + flip direction of fighters

--- Assets/Scripts/FighterController.cs
+++ Assets/Scripts/FighterController.cs
@@ -8,6 +8,7 @@ public class FighterController : MonoBehaviour
     //public bool test;
     private bool jumping;
     private Rigidbody2D m_Rigidbody;
+    [SerializeField] private SoundPlayer soundPlayer;
     [SerializeField] private Animator anim;
 
         // Start is called before the first frame update
@@ -16,10 +17,11 @@ public class FighterController : MonoBehaviour
         m_Rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    void CheckWalkDir(string newDir){
+    void ChangeDir(string newDir){
         if(newDir != ""){
             dir = newDir;
         }
+        transform.localScale = new Vector3(dir == "left" ? -1 : 1, 1, 1);
     }
 
     public void Stand(){
@@ -31,7 +33,7 @@ public class FighterController : MonoBehaviour
         //Vector3 targetPosition = Vector3.zero;
         //Vector3 targetMove;//?
         int speed = 1;
-        CheckWalkDir(newDir);
+        ChangeDir(newDir);
         // + transform.position;//or just have this saved?
 
         if(dir == "right"){
@@ -52,7 +54,7 @@ public class FighterController : MonoBehaviour
     public void Jump(string newDir){
         int speed = 1;
         anim.SetTrigger("jump");
-        CheckWalkDir(newDir);
+        ChangeDir(newDir);
 
         if(!jumping){
             m_Rigidbody.AddForce(new Vector2(0, 250));
@@ -81,7 +83,8 @@ public class FighterController : MonoBehaviour
     public void Punch(){
         anim.SetTrigger("punch");
     }
-    public void Dash(string dir){
+    public void Dash(string newDir){
+        ChangeDir(newDir);
         anim.SetTrigger("dash");
     }
 
