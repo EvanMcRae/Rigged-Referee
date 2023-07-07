2c3794b267eb8d19f1cbebd5fd90c6a375e22bc5
Awsomenova1 <91033039+Awsomenova1@users.noreply.github.com>
2023-07-07 15:16:08 -0400

    Added walk function to fighterControler

--- Assets/Scripts/FighterController.cs
+++ Assets/Scripts/FighterController.cs
@@ -5,14 +5,51 @@ using UnityEngine;
 public class FighterController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //void Start()
+    //{
+    //    
+    //}
+    public string dir;//the direction the fighter is currently moving
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    //void Update()
+    //{
+    //    Walk(dir);
+    //}
+
+    void CheckWalkDir(string newDir){
+        if(newDir != ""){
+            dir = newDir;
+        }
     }
+
+    public void Stand(){}
+
+    public void Walk(string newDir){
+        //Vector3 targetPosition = Vector3.zero;
+        //Vector3 targetMove;//?
+        int speed = 10;
+        CheckWalkDir(newDir);
+        // + transform.position;//or just have this saved?
+
+        if(dir == "right"){
+           transform.position = Vector3.MoveTowards(transform.position, transform.position + (speed*Vector3.right), speed * Time.deltaTime);
+        }
+        else if(dir == "left"){
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + (speed*Vector3.left), speed * Time.deltaTime);
+        }
+        else{
+            //do nothing, I suppose
+        }
+
+        //transform.position = Vector3.MoveTowards(transform.position, transform.position + targetMove, speed * Time.deltaTime);
+
+    }
+
+    public void Jump(string dir){}
+    public void Duck(){}
+    public void Punch(){}
+    public void Dash(string dir){}
+
+
 }
