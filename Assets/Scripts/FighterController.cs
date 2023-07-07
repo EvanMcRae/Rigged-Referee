e8b279b8cb1c9a030aa9f07906d37866c7c4c4ab
Awsomenova1 <91033039+Awsomenova1@users.noreply.github.com>
2023-07-07 16:10:54 -0400

    Added rigidbodies and attempted to add jump

--- Assets/Scripts/FighterController.cs
+++ Assets/Scripts/FighterController.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class FighterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    //void Start()
-    //{
-    //    
-    //}
     public string dir;//the direction the fighter is currently moving
+    //public bool test;
+    private bool jumping;
+    private Rigidbody2D m_Rigidbody;
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    Walk(dir);
-    //}
+        // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    //    
+    }
 
     void CheckWalkDir(string newDir){
         if(newDir != ""){
@@ -28,15 +27,16 @@ public class FighterController : MonoBehaviour
     public void Walk(string newDir){
         //Vector3 targetPosition = Vector3.zero;
         //Vector3 targetMove;//?
-        int speed = 10;
+        int speed = 1;
         CheckWalkDir(newDir);
         // + transform.position;//or just have this saved?
 
         if(dir == "right"){
-           transform.position = Vector3.MoveTowards(transform.position, transform.position + (speed*Vector3.right), speed * Time.deltaTime);
+           transform.position = Vector3.MoveTowards(transform.position, transform.position + (speed*Vector3.right), 1);
+           //transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
         }
         else if(dir == "left"){
-            transform.position = Vector3.MoveTowards(transform.position, transform.position + (speed*Vector3.left), speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + (speed*Vector3.left), 1);
         }
         else{
             //do nothing, I suppose
@@ -46,7 +46,20 @@ public class FighterController : MonoBehaviour
 
     }
 
-    public void Jump(string dir){}
+    public void Jump(string newDir){
+        int speed = 1;
+        CheckWalkDir(newDir);
+
+        if(!jumping){
+            m_Rigidbody.AddForce(new Vector2(0, 250));
+            jumping = true;
+        }
+        else{
+            if(m_Rigidbody.velocity.y == -250){
+                jumping = false;
+            }
+        }
+    }
     public void Duck(){}
     public void Punch(){}
     public void Dash(string dir){}
