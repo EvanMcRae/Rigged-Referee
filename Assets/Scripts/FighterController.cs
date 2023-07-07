f537c7fe51b3e2d1d58afb216b03f17ed5761a0f
EvanMcRae <evanmmcrae@gmail.com>
2023-07-07 14:22:45 -0400

    Rename fight interpreter script

--- Assets/Scripts/FighterController.cs
+++ Assets/Scripts/FighterController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FightInterpreter : MonoBehaviour
+public class FighterController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
