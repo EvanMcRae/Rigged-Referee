70e7ab659cbed1ac4c4edc30bb84b34b09a8a242
EvanMcRae <evanmmcrae@gmail.com>
2023-07-07 14:31:48 -0400

    Add 2D package + more parser progress

--- Assets/Scripts/FightParser.cs
+++ Assets/Scripts/FightParser.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class FightParser : MonoBehaviour
 {
     [SerializeField] private FighterController f1, f2;
-    [SerializeField] private File file;
+    [SerializeField] private string file;
+    private string script;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        script = File.ReadAllText(Path.Combine(Application.streamingAssetsPath + @"\FightScript\", file));
     }
 
     // Update is called once per frame
